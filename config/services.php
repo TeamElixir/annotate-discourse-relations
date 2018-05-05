e83d64a02ec9efb7911fd4ef6aa66c949b4b2eee
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-05-05 15:58:31 +0530

    feat: update services.php
    Add Google OAuth Details

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => 'https://fyp-oblie.herokuapp.com/dashboard',
+    ]
+
 ];
