3fe2359322aa56745af6823102585efbc74992b9
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-05-07 21:49:42 +0530

    feat: update auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,10 +61,12 @@
                                     <button type="submit" class="btn btn-primary">
                                         {{ __('Login') }}
                                     </button>
-
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <a href="{{url('register')}}">New User</a>
                                 </div>
                             </div>
                         </form>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <div>
+                            <strong>Logged in as:</strong> {{$user->name}} ({{$user->email}})
                         </div>
-                    @endif
-
-                    You are logged in!
+                        <br>
+                        <div>
+                            <a href="{{url('/')}}" class="btn btn-primary">Proceed to annotation</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
